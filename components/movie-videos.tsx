9b2596a1213c0766bd4871f0c593abbf8629c730
khv2644511 <khv2644511@naver.com>
2024-05-21 15:31:21 +0900

    Error Handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,8 +3,7 @@ import { API_URL } from '../app/(home)/page';
 async function getVideos(id: string) {
     console.log(`Fetching videos: ${Date.now()}`);
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
+    throw new Error('something broke...');
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
