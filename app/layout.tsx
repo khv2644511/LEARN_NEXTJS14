640373d973cb4d176f5298efcd88e6532fa19616
khv2644511 <khv2644511@naver.com>
2024-05-21 16:47:49 +0900

    css module

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,22 @@
-import { Metadata } from "next"
-import Navigation from "../components/navigation"
+import '../styles/global.css';
+import { Metadata } from 'next';
+import Navigation from '../components/navigation';
 
 export const metadata: Metadata = {
     title: {
-        template: "%s | Next Moviese",
-        default: 'Loading'
+        template: '%s | Next Moviese',
+        default: 'Loading',
     },
-  description: 'The bedt movies on thie best framework',
-}
+    description: 'The bedt movies on thie best framework',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>
-      <Navigation/>
-        {children}
-      </body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>
+                <Navigation />
+                {children}
+            </body>
+        </html>
+    );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,24 +1,27 @@
-"use client"
+'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import styles from '../styles/navigation.module.css';
 
 export default function Navigation() {
     const path = usePathname();
-    const [count, setCount ] = useState(0)
+    const [count, setCount] = useState(0);
     // console.log(path);
-    return (<nav>
-        <ul>
-            <li>
-                <Link href='/'>Home</Link> {path === '/' ? "🩷" : ""}
-            </li>
-            <li>
-                <Link href='/about-us'>About Us</Link> {path === '/about-us' ? "🩷" : ""}
-            </li>
-            <li>
-                <button onClick={()=> setCount((c) => c + 1)}>{count}</button>
-            </li>
-        </ul>
-    </nav>);
+    return (
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link> {path === '/' ? '🩷' : ''}
+                </li>
+                <li>
+                    <Link href="/about-us">About Us</Link> {path === '/about-us' ? '🩷' : ''}
+                </li>
+                <li>
+                    <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+                </li>
+            </ul>
+        </nav>
+    );
 }
