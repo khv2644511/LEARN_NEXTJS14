5f726d842d534781cc37785ee6e7dd6ada03c60f
khv2644511 <khv2644511@naver.com>
2024-05-27 15:35:40 +0900

    fix error

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,3 @@
-import { API_URL } from '../app/(home)/page';
-
 async function getVideos(id: string) {
     console.log(`Fetching videos: ${Date.now()}`);
     // await new Promise((resolve) => setTimeout(resolve, 3000));
