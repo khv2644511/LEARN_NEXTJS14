54d7e4f3de0c6fa9f3ed993b4ece1efaa57fc6e8
khv2644511 <khv2644511@naver.com>
2024-05-27 16:17:45 +0900

    style: Movie Detail page add Youtube video

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import MovieInfo, { getMovie } from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
 
 interface IParams {
     params: { id: string };
@@ -22,9 +23,9 @@ export default function MovieDetail({ params: { id } }: IParams) {
             <Suspense fallback={<h1>Loading movie info</h1>}>
                 <MovieInfo id={id} />
             </Suspense>
-            {/* <Suspense fallback={<h1>Loading movie vidieos</h1>}>
+            <Suspense fallback={<h1>Loading movie vidieos</h1>}>
                 <MovieVideos id={id} />
-            </Suspense> */}
+            </Suspense>
         </div>
     );
 }
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -13,7 +13,7 @@ export default async function MovieInfo({ id }: { id: string }) {
             <img src={movie.poster_path} className={styles.poster} alt={movie.title} />
             <div className={styles.info}>
                 <h1 className={styles.title}>{movie.ttile}</h1>
-                <h3>⭐️ {movie.vote_average}</h3>
+                <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
                 <p>{movie.overview}</p>
                 <a href={movie.homepage} target={'_blank'}>
                     Homepage &rarr;
